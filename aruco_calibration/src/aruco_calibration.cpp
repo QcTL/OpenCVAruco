5f14dcefd9b060e119c03d207604ccfd48d28d77
QcTL <71326643+QcTL@users.noreply.github.com>
2023-03-12 22:09:53 +0100

    Una mica més de documentació
    (falta el doble bucle for)

--- aruco_calibration/src/aruco_calibration.cpp
+++ aruco_calibration/src/aruco_calibration.cpp
@@ -20,9 +20,11 @@ int main(int argc, char* argv[]) {
     return -1;
   }
   
-  cv::VideoCapture webCam(std::atoi(argv[1]));       // VideoCapture object declaration. Usually 0 is the integrated, 2 is the first external USB one
+  //Com en l'exercici anterior tornem a capturar la camera amb la id que ens ha entrat l'usuari
+  cv::VideoCapture webCam(std::atoi(argv[1]));
 
-  if (webCam.isOpened() == false){   // Check if the VideoCapture object has been correctly associated to the webcam
+	//I comprobem que funciona i que és valida:
+  if (webCam.isOpened() == false){
     std::cerr << "error: Webcam could not be connected." << std::endl;
     return -1;
   }
@@ -37,7 +39,11 @@ int main(int argc, char* argv[]) {
 	string settingsOut = argv[7]; //Nom del fitxer que conté la calibració de la càmera.
  	
   char charCheckForESCKey = 0;
+  
+  //Comprobem que es fa prem la tecla C per fer captures
   char charCheckForCKey = 0;
+  
+  //Tornem a conseguir la id diccionari de la paraula entrada per l'usuari, mostrant un missatge si no surt.
   int at = 0;
   bool found = false;
   cout << "mirem diccionari" << endl;
@@ -50,11 +56,17 @@ int main(int argc, char* argv[]) {
 		cerr << "Dictionary " << dictSelected << " not found" << endl;
 		return -1;
   } 
-  cout << "caca 1" << endl;
+  //----
+  
+  //Creem el diccionari
  	cv::Ptr<cv::aruco::Dictionary> dictionary = cv::aruco::getPredefinedDictionary(at);
- 	cv::Ptr<cv::aruco::DetectorParameters> detectorParams = cv::aruco::DetectorParameters::create();
- 	cv::Ptr<cv::aruco::GridBoard> board = cv::aruco::GridBoard::create(nColumns, nRows, sideLong, marckerDist, dictionary);
- 	//FileStorage fs(detectorPara, FileStorage::READ); //Carreguem els paràmetres del detector.
+	//Creem la taula de Aruco que haurem de detectar amb les caracteristiques entrades per l'usuari
+	cv::Ptr<cv::aruco::GridBoard> board = cv::aruco::GridBoard::create(nColumns, nRows, sideLong, marckerDist, dictionary);
+ 	
+ 
+	cv::Ptr<cv::aruco::DetectorParameters> detectorParams = cv::aruco::DetectorParameters::create();
+ 
+	//FileStorage fs(detectorPara, FileStorage::READ); //Carreguem els paràmetres del detector.
   /*
   bool readOk = (*detectorParams)->readDetectorParameters(fs.root());
   
@@ -63,53 +75,64 @@ int main(int argc, char* argv[]) {
   	return -1;
   }
   */
-  cout << "caca 2" << endl;
-  std::vector<int> markerIds;
+  
+	//Com que tornem a fer detecció tornem a tenir un vector que contindrà les id dels arucos que surten en pantalla.
+	std::vector<int> markerIds;
 	std::vector<std::vector<cv::Point2f>> markerCorners, rejectedCandidates;
+	
+	//I tornem a tenir dos materials, el primer serà el que farem la detecció i el segon en el qual li posarem els grafics que indiquen
+	//Els contorns de les deteccions i quina id ha detectat.
 	cv::Mat inputImage, outputImage;
 	
 	//Variables per colectar la informacio de les imatges de calibració.
 	vector<vector<vector<Point2f>>> allCorners;
-  vector<vector<int>> allIds;
-  Size imgSize;
+	vector<vector<int>> allIds;
+	Size imgSize;
+	
+	//Nombre de captures
 	int nCapture = 0;
 	cout << "fem bucle" << endl;
 	while(charCheckForESCKey != 27 && webCam.isOpened()){
 		webCam.read(inputImage);
 		inputImage.copyTo(outputImage);
+		
+		//Tornem a detectat i mostrat els arucos detectats i la seva informació per pantalla.
 		cv::aruco::detectMarkers(inputImage, dictionary, markerCorners, markerIds, detectorParams, rejectedCandidates);
 		if (markerIds.size() > 0) cv::aruco::drawDetectedMarkers(outputImage, markerCorners, markerIds);
-		putText(outputImage, "Captures taken ( " + to_string(nCapture) + "/20)", Point(10, 20), FONT_HERSHEY_SIMPLEX, 0.5, Scalar(255, 0, 0), 2);
+		//També mostrem en la matriu de outputImage quantes captues s'han get:
+		putText(outputImage, "Fotos: ( " + to_string(nCapture) + "/20)", Point(10, 20), FONT_HERSHEY_SIMPLEX, 0.5, Scalar(255, 0, 0), 2);
 		
+		cv::imshow("out", outputImage);
 		
-		
-		
-    cv::imshow("out", outputImage);
-    
-    charCheckForCKey = cv::waitKey(1);  // gets the key pressed
-    cout << "id trobats: " << markerIds.size() << endl;
-    cout << "files*columnes" << nRows*nColumns << endl;
-    cout << "tecla apretada" << charCheckForCKey << endl;
+		charCheckForCKey = cv::waitKey(1);  // aconseguim quina tecla s'ha pres.
+	
+	
 		if(charCheckForCKey == 'c' && markerIds.size() == nRows*nColumns){ //Si cliques la tecla 'c' i s'han detectat totes les maqrques aruco
-			cout << "amongus" << endl;
+			
+			//Afeguim els punts del contorn de les marques aruco en un vector.
 			allCorners.push_back(markerCorners);
+			//També les seves id.
 			allIds.push_back(markerIds);	
 			imgSize = inputImage.size();
+			// I guardem la imatge que hem fotograficat.
 			cv::imwrite("Captura" + to_string(nCapture) + ".png", inputImage);
-			nCapture++;
+			nCapture++; //Agumentem el nombre de capturesa fetes
 		}
 		
-		charCheckForESCKey = cv::waitKey(1);  // gets the key pressed
+		charCheckForESCKey = cv::waitKey(1);
 	}
 	
-	if(nCapture<4){
+	
+	//Un cop processades totes: i pres la tecla ESC:
+	if(nCapture<4){ //Comprobem que hi haguin captures suficients
 		cout << "captures insuficients" << endl;
 		return -1;
 	}
 	
+	
 	Mat cameraMatrix, distCoeffs;
-  vector< Mat > rvecs, tvecs;
-  double repError;
+	vector< Mat > rvecs, tvecs;
+	double repError;
   
   vector<vector<Point2f>> allCornersConcatenated;
   vector<int> allIdsConcatenated;
@@ -123,21 +146,20 @@ int main(int argc, char* argv[]) {
       }
   }
   
-  // calibrate camera
+  //i calibrem la camera amb tots els punts i les marques fetes, això donerà una quantitat de distorsió que se li ha
+  //d'aplicar a la camera perque representi lo més proxim la realitat
   repError = aruco::calibrateCameraAruco(allCornersConcatenated, allIdsConcatenated,
                                          markerCounterPerFrame, board, imgSize, cameraMatrix,
                                          distCoeffs, rvecs, tvecs, 0);
 
+
+	//Tot seguit guaradrem aquesta informació trobada en un fitxer el qual s'ha entrat en els parametres finals
   bool saveOk = saveCameraParams(settingsOut, imgSize, 1, 0, cameraMatrix,
                                  distCoeffs, repError);
-
-  if(!saveOk) {
+  if(!saveOk) { //Mostrem si no s'ha pogut guradar el fitxer:
       cerr << "Cannot save output file" << endl;
-      return 0;
+      return -1;
   }
-
-  cout << "Rep Error: " << repError << endl;
-  cout << "Calibration saved to " << settingsOut << endl;
-	
+  
   return 0;
 }
