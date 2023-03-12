8b27ba9b0f1522c849c8a2a2ee77696be9758004
QcTL <71326643+QcTL@users.noreply.github.com>
2023-03-12 21:30:00 +0100

    + Explanation

--- generate_board/src/generate_board.cpp
+++ generate_board/src/generate_board.cpp
@@ -18,17 +18,25 @@ int main(int argc, char* argv[]) {
   }
   char charCheckForESCKey = 0;
   
-  cv::Mat imgAruBoard; // Matrix format image of the specified ID and size
+  cv::Mat imgAruBoard; // Matriu on guardarem la taula
+  
+  //Obtenim de la comanda de execució el nom de files i columnes
 	int nRows = stoi(argv[1]);
 	int nColumns = stoi(argv[2]);
 
-
+	//Tornem a obtenir el nom del diccionari entrat
   string dictSelected = argv[3];
  	
+	//Guardem la mida i la separació en metres, al entrar-se en pixels
   int sizeAruco = stoi(argv[4]) * 0.0002645833; 
 	int separationAruco = stoi(argv[5]) * 0.0002645833;
+	
+	//Tornarem a guardar on s'ha de guardar el resultat
   string imageName = argv[6];
   
+  
+  //Com en el codi de "generate_marker.cpp" crearem el diccionari amb la id, però per saber quina id correspont el diccionari que ens ha entrat l'usuari
+  //El compararem amb la llista que tenim
   int at = 0;
   bool found = false;
   
@@ -41,11 +49,14 @@ int main(int argc, char* argv[]) {
   cerr << "Dictionary " << dictSelected << " not found" << endl;
   return -1;
   } 
-  
+  //Guardem el diccionari trobat
   cv::Ptr<cv::aruco::Dictionary> dictionary = cv::aruco::getPredefinedDictionary(at);
+  //Generem la taula amb tots els atributs obtinguts
   cv::Ptr<cv::aruco::GridBoard> board = cv::aruco::GridBoard::create(nColumns, nRows, sizeAruco, separationAruco, dictionary);
   
+  //La dibuiexem en la matriu que em creat en l'inici
   board->draw( cv::Size(600, 500), imgAruBoard, 10, 1 );
+  //Tornarem a guardar la matriu com un fitxer en el nom entrat per l'usuari
   cv::imwrite(imageName, imgAruBoard);
   
   
