4176a93164287b50d8db4b7a8e715bbc6d8b07b4
QcTL <71326643+QcTL@users.noreply.github.com>
2023-03-12 21:38:26 +0100

    + Explicació

--- marker_detection/src/marker_detection.cpp
+++ marker_detection/src/marker_detection.cpp
@@ -18,17 +18,22 @@ int main(int argc, char* argv[]) {
     return -1;
   }
   
-  cv::VideoCapture webCam(std::atoi(argv[1]));       // VideoCapture object declaration. Usually 0 is the integrated, 2 is the first external USB one
+  // A causa que haurem de detectar els aruco amb la camera, la generarema amb la seguent linia.
+  //Amb la id que ha entrat l'usuari
+  cv::VideoCapture webCam(std::atoi(argv[1]));      
 
-  if (webCam.isOpened() == false){   // Check if the VideoCapture object has been correctly associated to the webcam
+//Comprobarem si hi podem accedir a la camera, sinò mostrarem un error.
+  if (webCam.isOpened() == false){ 
     std::cerr << "error: Webcam could not be connected." << std::endl;
     return -1;
   }
 
-  
+  //Guardem el nom del diccionari el qual voldrem detectar els aruco capturats per la camera.
   string dictSelected = argv[2];
  	
   char charCheckForESCKey = 0;
+  
+  //Tornem a fer el procces de obtenir el id del dicconari que l'usuari ha introduit
   int at = 0;
   bool found = false;
   
@@ -42,23 +47,36 @@ int main(int argc, char* argv[]) {
 		return -1;
   } 
   
-  /*
-  cv::Mat inputImage, outputImage;
- 	std::vector<int> markerIds;
-	std::vector<std::vector<cv::Point2f>> markerCorners, rejectedCandidates;
-  */
-  
  	cv::Ptr<cv::aruco::Dictionary> dictionary = cv::aruco::getPredefinedDictionary(at);
+	
+	//Generarem els Parametres basic per el detector de Arucos:
  	cv::Ptr<cv::aruco::DetectorParameters> detectorParams = cv::aruco::DetectorParameters::create();
+	
+	//Crearem el vector on es guardaran les Ids del arucos que seràn capturats per la camera, aquesta llista anirà canviant en cada frame d'execució.
   	std::vector<int> markerIds;
+	
+	//Guardarem un vector els 4 punts on la camera ha detectat Aruco i un altre de on la camera ha cregut que eren arucos pero han resultat que no eren
+	//Aquesta funció només la crearem per la funció de detecció i en aquest cas no li farem res.
 	std::vector<std::vector<cv::Point2f>> markerCorners, rejectedCandidates;
+	
+	//Creem la matriu que la camera hi projectarà i la altre matriu que serà la que es mostarà per pantalla que serà la camera més informació i posició
+	//sobre els arucos detectats
 	cv::Mat inputImage, outputImage;
+	
+	
 	while(charCheckForESCKey != 27 && webCam.isOpened()){
+		//Lleguim el frame de la camera i el guardem en la matriu.
 		webCam.read(inputImage);
+		//Copiem la matriu de la camera a la que es mostrarà per pantalla
 		inputImage.copyTo(outputImage);
+		//Detectem les marques aruco que hi ha en la camera que formin part del diccionari entrat
 		cv::aruco::detectMarkers(inputImage, dictionary, markerCorners, markerIds, detectorParams, rejectedCandidates);
+		
+		//Si hem detectat algun aruco, mostrarem la seva posició i la seva id en la matriu de outputImage.
 		if (markerIds.size() > 0) cv::aruco::drawDetectedMarkers(outputImage, markerCorners, markerIds);
-    		cv::imshow("out", outputImage);
+    	
+		//generem una finestra amb el nom de "out" i mostrarem la matriu que conté la informació important.
+		cv::imshow("out", outputImage);
 		charCheckForESCKey = cv::waitKey(1);  // gets the key pressed
 	}
 	
